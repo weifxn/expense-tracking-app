80c744eecdf6eb1287eb698db541795fa5abb72f
weifxn <weifan2647@gmail.com>
2018-10-06 14:17:56 +0800

    done modal

---
+++ components/MainModal.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import {
+  TextInput,
+  Text,
+  View,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from 'react-native';
+import Modal from 'react-native-modal';
+import CardButton from './CardButton'
+
+import styles from '../styles/Styles'
+
+export default class MainModal extends React.Component {
+  
+  render() {
+    return (
+      <Modal
+        isVisible={this.props.isVisible}
+        animationIn="slideInUp"
+        animationOut="slideOutDown"
+        onBackdropPress={this.props.onBackdropPress}
+        onSwipe={this.props.onSwipe}
+        swipeDirection="left"
+        >
+        <View style={{ flexDirection: 'column', alignItems: 'center' }}>
+          <View style={styles.card}> 
+            <Text style={styles.mainModalFont}>
+              I have
+            </Text>
+
+            <View style={{ flexDirection: 'row'}}>
+
+              <TouchableOpacity onPress={this.props.setIsSpent}>
+              <Text style={styles.mainModalFont}>
+                {this.props.isSpent ? 'spent' : 'received'} RM
+              </Text>
+              </TouchableOpacity>
+
+              <TextInput
+                style={styles.mainModalFont}
+                placeholder="0.00"
+                autoFocus={true}
+                keyboardType='decimal-pad'
+              />
+            </View>
+
+            <View style={{ flexDirection: 'row'}}>
+
+              <Text style={styles.mainModalFont}>
+                on{" "}
+              </Text>
+
+              <TextInput
+                style={styles.mainModalFont}
+                value={this.props.desc}
+                clearTextOnFocus={true}
+              />
+
+            </View>
+
+
+          </View>
+          <View style={{ flexDirection: 'row' }}>
+
+          <CardButton
+            name="Cancel"
+            style={styles.cardButtonSmallDelete}
+            onPress={this.props.onPressCancel}
+          />
+          <CardButton
+            name="Done"
+            style={styles.cardButtonSmall}
+            onPress={this.props.onPressDone}
+          />
+          </View>
+
+          
+        </View>
+
+        <KeyboardAvoidingView behavior="padding" />
+      </Modal>
+    );
+  }
+}
---
+++ styles/Styles.js
@@ -0,0 +1,102 @@
+import {StyleSheet, Dimensions} from 'react-native'
+
+const { heigh, width } = Dimensions.get('window');
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#7a77fb',
+    // justifyContent: 'center'
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  },
+  card: {
+    backgroundColor: '#fff',
+    margin: 4,
+    padding: 20,
+    width: width - 40,
+    borderRadius: 13,
+    shadowColor: 'rgb(50,50,50)',
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    shadowOffset: {
+      height: 3,
+      width: 0
+    }
+  },
+  input: {
+    textAlignVertical: 'top',
+    padding: 20,
+    fontSize: 24
+  },
+  inputDesc: {
+    textAlignVertical: 'top',
+    paddingTop: 20,
+    padding: 20,
+    fontSize: 18,
+    color: "#505051"
+  },
+  button: {
+    fontSize: 25,
+    padding: 20,
+    textAlign: 'center',
+    color: '#fff'
+  },
+  cardButton: {
+    backgroundColor: '#4b49b2',
+    margin: 6,
+    width: width - 50,
+    borderRadius: 13,
+    shadowColor: 'rgb(50,50,50)',
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    shadowOffset: {
+      height: 3,
+      width: 0
+    }
+  },
+  cardButtonSmall: {
+    backgroundColor: 'black',
+    margin: 6,
+    width: (width/2) - 25,
+    borderRadius: 13,
+    shadowColor: 'rgb(50,50,50)',
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    shadowOffset: {
+      height: 3,
+      width: 0
+    }
+  },
+  cardButtonSmallDelete: {
+    backgroundColor: '#f44259',
+    margin: 6,
+    width: (width/2) - 25,
+    borderRadius: 13,
+    shadowColor: 'rgb(50,50,50)',
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    shadowOffset: {
+      height: 3,
+      width: 0
+    }
+  },
+  cardSmall: {
+    backgroundColor: '#fff',
+    margin: 4,
+    width: (width/2) - 25,
+    borderRadius: 13,
+    shadowColor: 'rgb(50,50,50)',
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    shadowOffset: {
+      height: 3,
+      width: 0
+    }
+  },
+  mainModalFont: {
+    fontSize: 45,
+    fontWeight: 'bold'
+  }
+})
