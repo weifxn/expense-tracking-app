61d83f9b56c22271fdb95d54a119633f1fb230b9
weifxn <weifan2647@gmail.com>
2018-10-10 14:18:24 +0800

    fix backend

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Image,
   Platform,
@@ -10,25 +10,25 @@ import {
   Keyboard,
   Alert,
   AsyncStorage
-} from 'react-native';
-import { WebBrowser } from 'expo';
+} from "react-native";
+import { WebBrowser } from "expo";
 
-import { MonoText } from '../components/StyledText';
-import MainModal from '../components/MainModal';
-import ProgressBar from '../components/ProgressBar';
-import InitialRun from './initialPages/InitialRun';
-import Done from './initialPages/Done';
-import Main from './homePages/Main'
-import styles from '../styles/Styles'
+import { MonoText } from "../components/StyledText";
+import MainModal from "../components/MainModal";
+import ProgressBar from "../components/ProgressBar";
+import InitialRun from "./initialPages/InitialRun";
+import Done from "./initialPages/Done";
+import Main from "./homePages/Main";
+import styles from "../styles/Styles";
 
-import Moment from 'react-moment';
+import Moment from "react-moment";
 
-const cat = ['breakfast', 'lunch', 'dinner', 'other']
-var mealIndex = 2
+const cat = ["breakfast", "lunch", "dinner", "other"];
+var mealIndex = 2;
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
-    header: null,
+    header: null
   };
 
   state = {
@@ -42,35 +42,34 @@ export default class HomeScreen extends React.Component {
     isFirst: false,
 
     amount: null,
-    desc: '',
+    desc: "",
 
     data: [],
 
     todayData: [],
 
     monthAllow: null,
-    
+
     leftDays: null,
 
-    dayTotal: null,
-    dayLeft: null,
+    dayAllowanceTotal: null,
+    dayAllowanceLeft: null,
 
-    isMeal: '',
+    isMeal: "",
     defaultMealIndex: null
-  }
+  };
 
   componentDidMount = () => {
     var now = new Date();
     var _date = now.getDate();
     var hour = now.getHours();
-    var maxDays = new Date(now.getFullYear(), now.getMonth()+1, 0).getDate();
-    var _leftDays = maxDays - _date
-    
-    if(hour < 11) {
-      mealIndex = 0
-    }
-    else if (hour < 16) {
-      mealIndex = 1
+    var maxDays = new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
+    var _leftDays = maxDays - _date;
+
+    if (hour < 11) {
+      mealIndex = 0;
+    } else if (hour < 16) {
+      mealIndex = 1;
     }
 
     this.setState({
@@ -78,258 +77,250 @@ export default class HomeScreen extends React.Component {
       leftDays: _leftDays,
       isMeal: cat[mealIndex],
       defaultMealIndex: mealIndex
-    })
+    });
 
-    AsyncStorage.getItem('allReadyOK').then(itemsJSON => {
+    AsyncStorage.getItem("aaaa").then(itemsJSON => {
       if (itemsJSON === null) {
         this.setState({
           isFirst: true,
           isStart: false
-        })
+        });
       } else {
+        var storage = JSON.parse(itemsJSON);
+        console.log(JSON.stringify(storage[0]));
         this.setState({
-          data: JSON.parse(itemsJSON),
+          data: storage,
           modalVisible: true,
-          dayTotal: 50,
-          leftDays: 24,
-          dayLeft: 25.5,
-          todayData: [57.6,79.2,0,39.6]
-        })
-      }
-    })
-  }
+          monthAllow: storage[0][0].totalAllowance,
+          monthLeft: storage[0][0].totalAllowanceLeft,
+          leftDays: storage[0][0].leftDays,
+          dayAllowanceTotal: storage[0][0].dayAllowanceTotal,
+          dayAllowanceLeft: storage[0][0].dayAllowanceLeft,
 
-  // for saving 
-  save = item => {
-    AsyncStorage.setItem('allReadyOK', JSON.stringify(item));
+          todayData: storage[0][0].todayData,
+        });
+
+        console.log(JSON.stringify(storage[0][0].leftDays));
+        console.log(this.state.dayAllowanceLeft);
+      }
+    });
   };
 
-  // sort [todayData, dayLeft, dayTotal,dayofmonth,allowance]
+  // for saving
+  save = item => {
+    AsyncStorage.setItem("aaaa", JSON.stringify(item));
+  };
+
+  // sort [todayData, dayAllowanceLeft, dayTotal,dayofmonth,allowance]
   appendSave = item => {
-    const arr = [item, ...this.state.data]
-    this.save(arr)
-  }
-
-  saveRef = (one, two, three) => {
-    const arr = [...this.state.data, one, two, three]
-    this.save(arr)
-  }
-
+    const arr = [item, ...this.state.data];
+    this.save(arr);
+  };
 
   setIsFirst = () => {
     this.setState({
       isFirst: false,
-      todayData: [90,90,90,90]
-    })
-  }
+      todayData: [90, 90, 90, 90]
+    });
+  };
 
   setModalVisible(visible) {
-    if(!visible) {
-      Keyboard.dismiss()
+    if (!visible) {
+      Keyboard.dismiss();
     }
-    setTimeout(()=> {
-      this.setState({modalVisible: visible})
-    })
+    setTimeout(() => {
+      this.setState({ modalVisible: visible });
+    });
   }
 
-  
-
-  changeAmount = (num) => {
+  changeAmount = num => {
     this.setState({
       amount: num
-    })
-  }
+    });
+  };
 
   changeDesc = text => {
-
-    if(text !== '') {
+    if (text !== "") {
       this.setState({
         desc: text
-      })
+      });
     } else {
       this.setState({
         desc: this.state.isMeal
-      })
+      });
     }
-  }
-
+  };
 
   // initial part
   onDoneAllowance = () => {
-    if(this.state.monthAllow !== null) {
-      this.props.navigation.navigate('Two', {
+    if (this.state.monthAllow !== null) {
+      this.props.navigation.navigate("Two", {
         monthAllow: this.state.monthAllow,
         leftDays: this.state.leftDays,
         setIsFirst: this.setIsFirst
       });
     }
-  }
+  };
 
   onPressHistory = () => {
-    this.props.navigation.navigate('History')
-  }
+    this.props.navigation.navigate("History");
+  };
 
   onPressTransfer = () => {
-    this.props.navigation.navigate('Transfer')
-    this.setModalVisible(false)
-  }
+    this.props.navigation.navigate("Transfer");
+    this.setModalVisible(false);
+  };
   onPressWithdraw = () => {
-    this.props.navigation.navigate('Withdraw')
-    this.setModalVisible(false)
-  }
-
+    this.props.navigation.navigate("Withdraw");
+    this.setModalVisible(false);
+  };
 
   onChangeAllowance = num => {
     this.setState({
       monthAllow: num
-    })
-    const arr = [num]
-    this.save(arr)
-  }
-
+    });
+    const arr = [num];
+    this.save(arr);
+  };
 
   showDate = () => {
-    
-    if(this.state.desc === "breakfast") {
-      mealIndex = 0
+    if (this.state.desc === "breakfast") {
+      mealIndex = 0;
+    } else if (this.state.desc === "lunch") {
+      mealIndex = 1;
+    } else if (this.state.desc === "dinner") {
+      mealIndex = 2;
+    } else if (this.state.desc === "") {
+      mealIndex = this.state.defaultMealIndex;
+    } else {
+      mealIndex = 3;
     }
-    else if (this.state.desc === "lunch") {
-      mealIndex = 1
-    }
-    else if (this.state.desc === "dinner") {
-      mealIndex = 2
-    }
-    else if (this.state.desc === "") {
-      mealIndex = this.state.defaultMealIndex
-    }
-    else {
-      mealIndex = 3
-    }
-  
 
-    var amt = this.state.amount
-    var left = this.state.dayLeft
-    var total = this.state.dayTotal
-    var perc = (amt/total) * 360
-    
-    var amtLeft = parseFloat(left - amt).toFixed(2)
-      
-    
+    var amt = this.state.amount;
+    var left = this.state.dayAllowanceLeft;
+    var total = this.state.dayAllowanceTotal;
+    var perc = (amt / total) * 360;
+
+    var amtLeft = parseFloat(left - amt).toFixed(2);
+
     var now = new Date();
-    var maxDate = new Date(now.getFullYear(), now.getMonth()+1, 0).getDate()
+    var maxDate = new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
     var date = now.getDate();
 
-    var newTodayData = this.state.todayData
-    if(mealIndex === 3) {
-      newTodayData[mealIndex] = perc + newTodayData[mealIndex]
+    var newTodayData = this.state.todayData;
+    if (mealIndex === 3) {
+      newTodayData[mealIndex] = perc + newTodayData[mealIndex];
     } else {
-      newTodayData[mealIndex] = perc
+      newTodayData[mealIndex] = perc;
     }
 
+    var monthLeft = this.state.monthAllow - this.state.dayAllowanceTotal;
 
-
-    var dleft = maxDate - date
+    var dleft = maxDate - date;
 
     this.setState({
-        date: date,
+      date: date,
+      leftDays: dleft,
+      todayData: newTodayData,
+      dayAllowanceLeft: amtLeft,
+      amount: null
+    });
+
+    this.appendSave(this.state.todayData);
+
+    var forStorage = [
+      {
+        totalAllowance: this.state.monthAllow,
+        totalAllowanceLeft: monthLeft,
         leftDays: dleft,
-        todayData: newTodayData,
-        dayLeft: amtLeft,
-        amount: null
-    })
+        dayAllowanceTotal: total,
+        dayAllowanceLeft: amtLeft,
+        todayData: this.state.todayData
+      }
+    ];
 
-    this.saveRef(amtLeft, total, date, this.state.monthAllow)
-    this.appendSave(this.state.todayData)
+    console.log(JSON.stringify(forStorage));
 
-   
-    var spent = 1000 / left
+    var spent = 1000 / left;
 
+    this.appendSave(forStorage);
 
-    this.setModalVisible(false)
-  }
-
+    this.setModalVisible(false);
+  };
 
   render() {
     var month = new Date().getMonth() + 1;
     var year = new Date().getFullYear();
     return (
       <View style={styles.container}>
-      {this.state.isStart ? (
-        <Main 
-          howMuch={parseFloat(this.state.dayLeft).toFixed(2)}
-          onPressDone={()=>this.setModalVisible(true)}
-          one={this.state.todayData[0]}
-          two={this.state.todayData[1]}
-          three={this.state.todayData[2]}
-          four={this.state.todayData[3]}
-          onPress={()=>this.onPressHistory()}
-          days={this.state.leftDays}
-          multiplyNew={parseFloat((this.state.leftDays)*(this.state.dayLeft)).toFixed(2)}
-
-          multiply={parseFloat(((this.state.leftDays)-10)*(this.state.dayLeft)).toFixed(2)}
-        />
-        ) : (
-
-        <View>
-          
-          {this.state.isFirst ? (
-            <View>
-            <InitialRun 
-              changeText={this.onChangeAllowance}
-              onPressDone={()=>this.onDoneAllowance()}
-            />
-            </View>
-            ) : (
-
-            <Done
-              one={this.state.todayData[0]}
-              two={this.state.todayData[1]}
-              three={this.state.todayData[2]}
-              four={this.state.todayData[3]}
-              monthLeft={this.props.navigation.getParam('monthLeft')}
-              leftDays={this.state.leftDays} 
-              dayTotal={this.props.navigation.getParam('dayTotal')}
-              onPressDone={()=>{
-                this.setState({
-                isStart: true,
-                dayTotal: this.props.navigation.getParam('dayTotal'),
-                dayLeft: this.props.navigation.getParam('dayTotal'),
-                todayData: [0,0,0,0]
-              })
-              }}
-            />
-
-          )}
-
-        </View>
-
-        )}
-            
-
-          <MainModal
-            isVisible={this.state.modalVisible}
-            onSwipe={() => this.setModalVisible(false)}
-            onBackdropPress={() => this.setModalVisible(false)}
-            isSpent={this.state.isSpent}
-            setIsSpent={() => this.setState({ isSpent: !this.state.isSpent})}
-            onPressCancel={() => this.setModalVisible(false)}
-            
-            onPressDone={()=>this.showDate()}
-
-
-            descValue={this.state.isMeal}
-            descOnChangeText={this.changeDesc}
-
-            moneyValue={this.state.amount}
-            moneyOnChangeText={this.changeAmount}
-
-            onPressTransfer={()=>this.onPressTransfer()}
-            onPressWithdraw={()=>this.onPressWithdraw()}
-
+        {this.state.isStart ? (
+          <Main
+            howMuch={parseFloat(this.state.dayAllowanceLeft).toFixed(2)}
+            onPressDone={() => this.setModalVisible(true)}
+            one={this.state.todayData[0]}
+            two={this.state.todayData[1]}
+            three={this.state.todayData[2]}
+            four={this.state.todayData[3]}
+            onPress={() => this.onPressHistory()}
+            days={this.state.leftDays}
+            multiplyNew={parseFloat(
+              this.state.leftDays * this.state.dayAllowanceLeft
+            ).toFixed(2)}
+            multiply={parseFloat(
+              (this.state.leftDays - 10) * this.state.dayAllowanceLeft
+            ).toFixed(2)}
           />
+        ) : (
+          <View>
+            {this.state.isFirst ? (
+              <View>
+                <InitialRun
+                  changeText={this.onChangeAllowance}
+                  onPressDone={() => this.onDoneAllowance()}
+                />
+              </View>
+            ) : (
+              <Done
+                one={this.state.todayData[0]}
+                two={this.state.todayData[1]}
+                three={this.state.todayData[2]}
+                four={this.state.todayData[3]}
+                monthLeft={this.props.navigation.getParam("monthLeft")}
+                leftDays={this.state.leftDays}
+                dayTotal={this.props.navigation.getParam("dayTotal")}
+                onPressDone={() => {
+                  this.setState({
+                    isStart: true,
+                    dayAllowanceTotal: this.props.navigation.getParam(
+                      "dayTotal"
+                    ),
+                    dayAllowanceLeft: this.props.navigation.getParam(
+                      "dayTotal"
+                    ),
+                    todayData: [0, 0, 0, 0]
+                  });
+                }}
+              />
+            )}
+          </View>
+        )}
+
+        <MainModal
+          isVisible={this.state.modalVisible}
+          onSwipe={() => this.setModalVisible(false)}
+          onBackdropPress={() => this.setModalVisible(false)}
+          isSpent={this.state.isSpent}
+          setIsSpent={() => this.setState({ isSpent: !this.state.isSpent })}
+          onPressCancel={() => this.setModalVisible(false)}
+          onPressDone={() => this.showDate()}
+          descValue={this.state.isMeal}
+          descOnChangeText={this.changeDesc}
+          moneyValue={this.state.amount}
+          moneyOnChangeText={this.changeAmount}
+          onPressTransfer={() => this.onPressTransfer()}
+          onPressWithdraw={() => this.onPressWithdraw()}
+        />
       </View>
     );
   }
 }
-
-
